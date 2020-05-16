a1202e386dc7a4bba46819e85b461fd5519e8df6
Ger0505 <gerzonex360@gmail.com>
2020-05-16 14:39:06 -0500

    BD added

---
+++ models/_createCFE.sql
@@ -0,0 +1,116 @@
+# ---------------------------------------------------------------------- #
+# Script generated with: DeZign for Databases v6.2.1                     #
+# Target DBMS:           MySQL 5                                         #
+# Project file:          CFE.dez                                         #
+# Project name:                                                          #
+# Author:                                                                #
+# Script type:           Database creation script                        #
+# Created on:            2020-05-16 14:32                                #
+# ---------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------- #
+# Tables                                                                 #
+# ---------------------------------------------------------------------- #
+
+# ---------------------------------------------------------------------- #
+# Add table "COLABORADOR"                                                #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `COLABORADOR` (
+    `IdColaborador` INTEGER NOT NULL AUTO_INCREMENT,
+    `Nombre` VARCHAR(40) NOT NULL,
+    `Apellido1` VARCHAR(40) NOT NULL,
+    `Apellido2` VARCHAR(40),
+    `FechaAlta` DATE NOT NULL,
+    `Usuario` VARCHAR(40) NOT NULL,
+    `Password` VARCHAR(40) NOT NULL,
+    `IdPuesto` INTEGER NOT NULL,
+    `IdDepartamento` INTEGER NOT NULL,
+    `Status` INTEGER NOT NULL DEFAULT 1,
+    CONSTRAINT `PK_COLABORADOR` PRIMARY KEY (`IdColaborador`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "PUESTO"                                                     #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `PUESTO` (
+    `IdPuesto` INTEGER NOT NULL AUTO_INCREMENT,
+    `Descripcion` VARCHAR(40) NOT NULL,
+    CONSTRAINT `PK_PUESTO` PRIMARY KEY (`IdPuesto`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "DEPARTAMENTO"                                               #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `DEPARTAMENTO` (
+    `IdDepartamento` INTEGER NOT NULL AUTO_INCREMENT,
+    `Descripcion` VARCHAR(60) NOT NULL,
+    CONSTRAINT `PK_DEPARTAMENTO` PRIMARY KEY (`IdDepartamento`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "TAREA"                                                      #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `TAREA` (
+    `IdTarea` INTEGER NOT NULL AUTO_INCREMENT,
+    `TItulo` VARCHAR(40) NOT NULL,
+    `Descripcion` VARCHAR(60),
+    `FechaInicio` DATE NOT NULL,
+    `FechaFin` DATE NOT NULL,
+    `IdResponsable` INTEGER NOT NULL,
+    `IdColaborador` INTEGER,
+    `IdStatus` INTEGER NOT NULL,
+    CONSTRAINT `PK_TAREA` PRIMARY KEY (`IdTarea`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "STATUS"                                                     #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `STATUS` (
+    `IdStatus` INTEGER NOT NULL AUTO_INCREMENT,
+    `Descripcion` VARCHAR(40) NOT NULL,
+    CONSTRAINT `PK_STATUS` PRIMARY KEY (`IdStatus`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "COMENTARIOS"                                                #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `COMENTARIOS` (
+    `IdComentario` INTEGER NOT NULL AUTO_INCREMENT,
+    `Comentario` VARCHAR(100) NOT NULL,
+    `Fecha` DATE NOT NULL,
+    `IdTarea` INTEGER NOT NULL,
+    `IdColaborador` INTEGER NOT NULL,
+    CONSTRAINT `PK_COMENTARIOS` PRIMARY KEY (`IdComentario`)
+);
+
+# ---------------------------------------------------------------------- #
+# Foreign key constraints                                                #
+# ---------------------------------------------------------------------- #
+
+ALTER TABLE `COLABORADOR` ADD CONSTRAINT `PUESTO_COLABORADOR` 
+    FOREIGN KEY (`IdPuesto`) REFERENCES `PUESTO` (`IdPuesto`);
+
+ALTER TABLE `COLABORADOR` ADD CONSTRAINT `DEPARTAMENTO_COLABORADOR` 
+    FOREIGN KEY (`IdDepartamento`) REFERENCES `DEPARTAMENTO` (`IdDepartamento`);
+
+ALTER TABLE `TAREA` ADD CONSTRAINT `COLABORADOR_TAREAResponsable` 
+    FOREIGN KEY (`IdResponsable`) REFERENCES `COLABORADOR` (`IdColaborador`);
+
+ALTER TABLE `TAREA` ADD CONSTRAINT `STATUS_TAREA` 
+    FOREIGN KEY (`IdStatus`) REFERENCES `STATUS` (`IdStatus`);
+
+ALTER TABLE `TAREA` ADD CONSTRAINT `COLABORADOR_TAREAColaborador` 
+    FOREIGN KEY (`IdColaborador`) REFERENCES `COLABORADOR` (`IdColaborador`);
+
+ALTER TABLE `COMENTARIOS` ADD CONSTRAINT `TAREA_COMENTARIOS` 
+    FOREIGN KEY (`IdTarea`) REFERENCES `TAREA` (`IdTarea`);
+
+ALTER TABLE `COMENTARIOS` ADD CONSTRAINT `COLABORADOR_COMENTARIOS` 
+    FOREIGN KEY (`IdColaborador`) REFERENCES `COLABORADOR` (`IdColaborador`);
---
+++ models/_dropCFE.sql
@@ -0,0 +1,126 @@
+# ---------------------------------------------------------------------- #
+# Script generated with: DeZign for Databases v6.2.1                     #
+# Target DBMS:           MySQL 5                                         #
+# Project file:          CFE.dez                                         #
+# Project name:                                                          #
+# Author:                                                                #
+# Script type:           Database drop script                            #
+# Created on:            2020-05-16 14:32                                #
+# ---------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------- #
+# Drop foreign key constraints                                           #
+# ---------------------------------------------------------------------- #
+
+ALTER TABLE `COLABORADOR` DROP FOREIGN KEY `PUESTO_COLABORADOR`;
+
+ALTER TABLE `COLABORADOR` DROP FOREIGN KEY `DEPARTAMENTO_COLABORADOR`;
+
+ALTER TABLE `TAREA` DROP FOREIGN KEY `COLABORADOR_TAREAResponsable`;
+
+ALTER TABLE `TAREA` DROP FOREIGN KEY `STATUS_TAREA`;
+
+ALTER TABLE `TAREA` DROP FOREIGN KEY `COLABORADOR_TAREAColaborador`;
+
+ALTER TABLE `COMENTARIOS` DROP FOREIGN KEY `TAREA_COMENTARIOS`;
+
+ALTER TABLE `COMENTARIOS` DROP FOREIGN KEY `COLABORADOR_COMENTARIOS`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "COMENTARIOS"                                               #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `COMENTARIOS` MODIFY `IdComentario` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `COMENTARIOS` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `COMENTARIOS`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "TAREA"                                                     #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `TAREA` MODIFY `IdTarea` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `TAREA` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `TAREA`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "COLABORADOR"                                               #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `COLABORADOR` MODIFY `IdColaborador` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `COLABORADOR` ALTER COLUMN `Status` DROP DEFAULT;
+
+ALTER TABLE `COLABORADOR` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `COLABORADOR`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "STATUS"                                                    #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `STATUS` MODIFY `IdStatus` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `STATUS` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `STATUS`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "DEPARTAMENTO"                                              #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `DEPARTAMENTO` MODIFY `IdDepartamento` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `DEPARTAMENTO` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `DEPARTAMENTO`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "PUESTO"                                                    #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `PUESTO` MODIFY `IdPuesto` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `PUESTO` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `PUESTO`;
